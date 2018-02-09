abc3fef6f166110fd4b3f33a06ebf7362b0ae284
Francois Durand <fradurand@gmail.com>
2018-02-09 08:42:47 +0100

    Add MyClass and MyClass2 to __init__

--- hanabython/__init__.py
+++ hanabython/__init__.py
@@ -1,7 +1,10 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 
 """Top-level package for Hanabython."""
 
 __author__ = """François Durand"""
 __email__ = 'fradurand@gmail.com'
 __version__ = '0.1.0'
+
+from MyClass import MyClass
+from MyClass2 import MyClass2
