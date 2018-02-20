ab9413e30a13e4d6adcdae4ac83879bf99742d55
Francois Durand <fradurand@gmail.com>
2018-02-20 01:58:04 +0100

    Test RTD

--- hanabython/__init__.py
+++ hanabython/__init__.py
@@ -7,6 +7,5 @@ __email__ = 'fradurand@gmail.com'
 __version__ = '0.1.0'
 
 from .Classes.PrintColor import PrintColor
+from .Classes.StringUtils import uncolor, title
 from .Classes.Colored import Colored
-from .Classes.Color import Color
-from .Classes.StringUtils import uncolor
