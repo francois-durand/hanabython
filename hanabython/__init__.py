233fba9f19eeceec4401b67b19f742012660354b
Francois Durand <fradurand@gmail.com>
2018-02-26 09:52:38 +0100

    Test change import format for PyPI release

--- hanabython/__init__.py
+++ hanabython/__init__.py
@@ -6,35 +6,37 @@ __author__ = """François Durand"""
 __email__ = 'fradurand@gmail.com'
 __version__ = '0.1.0'
 
-from .Modules.Action import Action
-from .Modules.ActionClue import ActionClue
-from .Modules.ActionThrow import ActionThrow
-from .Modules.ActionForfeit import ActionForfeit
-from .Modules.ActionPlayCard import ActionPlayCard
-from .Modules.Board import Board
-from .Modules.Card import Card
-from .Modules.CardPublic import CardPublic
-from .Modules.Clue import Clue
-from .Modules.Color import Color
-from .Modules.ColorMulticolor import ColorMulticolor
-from .Modules.ColorColorless import ColorColorless
-from .Modules.Colors import Colors
-from .Modules.Colored import Colored
-from .Modules.Configuration import Configuration
-from .Modules.ConfigurationColorContents import ConfigurationColorContents
-from .Modules.ConfigurationDeck import ConfigurationDeck
-from .Modules.ConfigurationEmptyClueRule import ConfigurationEmptyClueRule
-from .Modules.ConfigurationEndRule import ConfigurationEndRule
-from .Modules.ConfigurationHandSize import ConfigurationHandSize
-from .Modules.DiscardPile import DiscardPile
-from .Modules.DrawPile import DrawPile
-from .Modules.DrawPilePublic import DrawPilePublic
-from .Modules.Game import Game
-from .Modules.Hand import Hand
-from .Modules.HandPublic import HandPublic
-from .Modules.Player import Player
-from .Modules.PlayerBase import PlayerBase
-from .Modules.PlayerHumanText import PlayerHumanText
-from .Modules.PlayerPuppet import PlayerPuppet
-from .Modules.StringAnsi import StringAnsi
-from .Modules.StringUtils import uncolor, title, str_from_iterable
+from hanabython.Modules.Action import Action
+from hanabython.Modules.ActionClue import ActionClue
+from hanabython.Modules.ActionThrow import ActionThrow
+from hanabython.Modules.ActionForfeit import ActionForfeit
+from hanabython.Modules.ActionPlayCard import ActionPlayCard
+from hanabython.Modules.Board import Board
+from hanabython.Modules.Card import Card
+from hanabython.Modules.CardPublic import CardPublic
+from hanabython.Modules.Clue import Clue
+from hanabython.Modules.Color import Color
+from hanabython.Modules.ColorMulticolor import ColorMulticolor
+from hanabython.Modules.ColorColorless import ColorColorless
+from hanabython.Modules.Colors import Colors
+from hanabython.Modules.Colored import Colored
+from hanabython.Modules.Configuration import Configuration
+from hanabython.Modules.ConfigurationColorContents \
+    import ConfigurationColorContents
+from hanabython.Modules.ConfigurationDeck import ConfigurationDeck
+from hanabython.Modules.ConfigurationEmptyClueRule \
+    import ConfigurationEmptyClueRule
+from hanabython.Modules.ConfigurationEndRule import ConfigurationEndRule
+from hanabython.Modules.ConfigurationHandSize import ConfigurationHandSize
+from hanabython.Modules.DiscardPile import DiscardPile
+from hanabython.Modules.DrawPile import DrawPile
+from hanabython.Modules.DrawPilePublic import DrawPilePublic
+from hanabython.Modules.Game import Game
+from hanabython.Modules.Hand import Hand
+from hanabython.Modules.HandPublic import HandPublic
+from hanabython.Modules.Player import Player
+from hanabython.Modules.PlayerBase import PlayerBase
+from hanabython.Modules.PlayerHumanText import PlayerHumanText
+from hanabython.Modules.PlayerPuppet import PlayerPuppet
+from hanabython.Modules.StringAnsi import StringAnsi
+from hanabython.Modules.StringUtils import uncolor, title, str_from_iterable
